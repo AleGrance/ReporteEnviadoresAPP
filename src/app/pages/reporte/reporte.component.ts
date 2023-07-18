9375fb12ac22abadc7cfa02515f55eab33673827
Tecnico <Tecnico@NB-INFOR-001006.FUTURO.LOCAL>
2023-07-18 10:52:07 -0400

    add spinner to the rows amount

--- src/app/pages/reporte/reporte.component.ts
+++ src/app/pages/reporte/reporte.component.ts
@@ -31,52 +31,83 @@ export class ReporteComponent implements OnInit {
   ngOnInit(): void {}
 
   getTickets() {
+    // Se muestra el spinner al hacer click en el btn
+    let cant_tickets = document.getElementById('cant_tickets');
+    cant_tickets?.insertAdjacentHTML('afterbegin', `<div class="spinner-border spinner-border-sm" role="status">
+    <span class="visually-hidden"></span>
+  </div>`);
+
     // Tickets
     this.apiTickets
       .get('ticketsNotificados')
       .pipe(
         map((data) => {
           this.cantTickets = data;
-          console.log(this.cantTickets);
+          // Se reemplaza el spinner por el dato obtenido
+          cant_tickets?.replaceChildren(this.cantTickets);
         })
       )
       .subscribe();
   }
 
   getNoAsistidos() {
+    // Se muestra el spinner al hacer click en el btn
+    let cant_noasistidos = document.getElementById('cant_noasistidos');
+    cant_noasistidos?.insertAdjacentHTML('afterbegin', `<div class="spinner-border spinner-border-sm" role="status">
+    <span class="visually-hidden"></span>
+  </div>`);
+
     // No Asistidos
     this.apiNoAsistidos
       .get('turnosNoAsistidosNotificados')
       .pipe(
         map((data) => {
           this.cantNoAsistidos = data;
-          console.log(this.cantNoAsistidos);
+          //console.log(this.cantNoAsistidos);
+          // Se reemplaza el spinner por el dato obtenido
+          cant_noasistidos?.replaceChildren(this.cantNoAsistidos);
         })
       )
       .subscribe();
   }
 
   get48Hs() {
+    // Se muestra el spinner al hacer click en el btn
+    let cant_48hs = document.getElementById('cant_48hs');
+    cant_48hs?.insertAdjacentHTML('afterbegin', `<div class="spinner-border spinner-border-sm" role="status">
+    <span class="visually-hidden"></span>
+  </div>`);
+
     // 48hs
     this.api48Hs
       .get('turnos48Notificados')
       .pipe(
         map((data) => {
           this.cant48hs = data;
-          console.log(this.cant48hs);
+          //console.log(this.cant48hs);
+          // Se reemplaza el spinner por el dato obtenido
+          cant_48hs?.replaceChildren(this.cant48hs);
         })
       )
       .subscribe();
   }
 
   getSucursales48Hs() {
+    // Se muestra el spinner al hacer click en el btn
+    let cant_sucursales48hs = document.getElementById('cant_sucursales48hs');
+    cant_sucursales48hs?.insertAdjacentHTML('afterbegin', `<div class="spinner-border spinner-border-sm" role="status">
+    <span class="visually-hidden"></span>
+  </div>`);
+
     // Sucursales Sucursales 48hs
     this.apiSucursales48Hs
       .get('turnosSucursales48hsNotificados')
       .pipe(
         map((data) => {
           this.cantSucursales48hs = data;
-          console.log(this.cantSucursales48hs);
+          //console.log(this.cantSucursales48hs);
+          // Se reemplaza el spinner por el dato obtenido
+          cant_sucursales48hs?.replaceChildren(this.cantSucursales48hs);
         })
       )
       .subscribe();
